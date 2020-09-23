5bb08178437db41da8d50e96b808d17326b8ba45
dong-soo <68644276+dong-soo@users.noreply.github.com>
2020-09-23 23:36:15 +0900

    문자열 다루기

---
+++ Data_and_operation/src/StringOperation.java
@@ -0,0 +1,14 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		//length 글자수 명령어
+		System.out.println("Hello World".length());
+		
+		//repace a를 b로 바꿔줘! (name,a / dongsoo,b) / 이름만바꿔서 반복할때 좋음
+		System.out.println("Hello, [[[name]]] ...bye. ".replace("[[[name]]]", "DongSoo"));
+		
+	}
+
+}
