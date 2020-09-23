2dcf9f4a27772bf996011cd30caf6655800df284
dong-soo <68644276+dong-soo@users.noreply.github.com>
2020-09-23 23:35:44 +0900

    문자열 다루기

---
+++ Data_and_operation/src/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow World"); // String
+		System.out.println('H'); // Character(한글자를 쓰는 캐릭터, 캐릭터가 모여 String) 
+		System.out.println("H"); // 무방함
+
+		System.out.println("Hellow "
+				+ "World"); //
+		// (\n) new ling 줄바꿈 명령어  역슬래시 엔터옆 / 
+	System.out.println("Hellow \nWorld");
+	
+	// escape 
+	System.out.println("Hellow \"World\""); // Hello "World"
+	}
+
+}
